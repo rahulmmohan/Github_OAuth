fbb2461ba65090d007ad9d7ceaa60fe11a0a627d
Rahul Mohan <rahul.mohan@CS-113-LT-MAC.local>
2018-04-30 16:11:21 +0530

    Initial commit

---
+++ app/src/main/java/oauth/github/sample/app/github_oauth/MainActivity.kt
@@ -0,0 +1,12 @@
+package oauth.github.sample.app.github_oauth
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ github-oauth/src/main/java/com/github/rahul/github_oauth/GithubOAuthActivity.kt
@@ -0,0 +1,12 @@
+package com.github.rahul.github_oauth
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GithubOAuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_github_oauth)
+    }
+}
