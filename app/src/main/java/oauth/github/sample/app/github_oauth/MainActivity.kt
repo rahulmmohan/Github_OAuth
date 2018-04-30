2664787af102dd7797d49326655bb16b2e0fe5ea
rahul.mohan <rahulmohanpgr@gmail.com>
2018-04-30 18:36:03 +0530

    github fetch tocken from code

--- app/src/main/java/oauth/github/sample/app/github_oauth/MainActivity.kt
+++ app/src/main/java/oauth/github/sample/app/github_oauth/MainActivity.kt
@@ -1,12 +1,31 @@
 package oauth.github.sample.app.github_oauth
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.github.rahul.github_oauth.Constants
+import com.github.rahul.github_oauth.GithubOAuthActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, GithubOAuthActivity::class.java)
+        intent.putExtra("id", "fbd747cbb00a75eda14f")
+        intent.putExtra("secret", "38f6c51cf1d6b9df16603a03d16a8a9da970970b")
+        startActivityForResult(intent, Constants.REQUEST_CODE)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Constants.REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Toast.makeText(this, "token received: " + data!!.getStringExtra(Constants.TOKEN), Toast.LENGTH_LONG).show()
+        }
     }
 }
--- github-oauth/src/main/java/com/github/rahul/github_oauth/Constants.kt
+++ github-oauth/src/main/java/com/github/rahul/github_oauth/Constants.kt
@@ -1,8 +1,11 @@
 package com.github.rahul.github_oauth
 
- object Constants {
+object Constants {
 
-     val GITHUB_URL = "https://github.com/login/oauth/authorize"
-     val GITHUB_OAUTH = "https://github.com/login/oauth/access_token"
-     val REQUEST_CODE = 1000
+    val GITHUB_URL = "https://github.com/login/oauth/authorize"
+    val GITHUB_OAUTH = "https://github.com/login/oauth/access_token"
+    val TOKEN = "TOKEN"
+    val REQUEST_CODE = 1000
+    val SUCCESS = -1
+    val ERROR = 0
 }
