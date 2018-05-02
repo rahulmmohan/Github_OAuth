a70c4a1898269d9110aa0ba11df4d52a5e2cc857
rahulmmohan <rahulmohanpgr@gmail.com>
2018-05-02 22:36:31 +0530

    client_id, client_secret from buildConfig

--- app/src/main/java/oauth/github/sample/app/github_oauth/MainActivity.kt
+++ app/src/main/java/oauth/github/sample/app/github_oauth/MainActivity.kt
@@ -3,19 +3,18 @@ package oauth.github.sample.app.github_oauth
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
-import com.github.rahul.githuboauth.ErrorCallback
-import com.github.rahul.githuboauth.GithubAuthenticator
-import com.github.rahul.githuboauth.SuccessCallback
+import com.github.rahul.githuboauth.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    val GITHUB_ID = BuildConfig.GITHUB_ID
+    val GITHUB_SECRET = BuildConfig.GITHUB_SECRET
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val build = GithubAuthenticator.builder(this)
-                .clientId("fbd747cbb00a75eda14f")
-                .clientSecret("38f6c51cf1d6b9df16603a03d16a8a9da970970b")
+        val githubAuthenticatorBuilder = GithubAuthenticator.builder(this)
+                .clientId(GITHUB_ID)
+                .clientSecret(GITHUB_SECRET)
                 .scopeList(arrayListOf("gist", "repo"))
                 .debug(true)
                 .onSuccess(object : SuccessCallback {
@@ -34,10 +33,11 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
                 })
-                .build()
+        githubAuthenticatorBuilder.debug(true)
+        val githubAuthenticator = githubAuthenticatorBuilder.build()
 
         login.setOnClickListener {
-            build.authenticate()
+            githubAuthenticator.authenticate()
         }
 
 
